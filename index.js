dd0358a2b68925a96119f901f9a69ab682ffe6dc
Alex <chernysh2909@gmail.com>
2020-01-20 15:47:28 +0200

    vf

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
+const json2xls = require('json2xls');
 
-const URL = 'https://auto.ria.com/newauto/marka-volkswagen/';
+const URL = 'https://auto.ria.com/newauto/marka-audi/';
 const HOST = 'https://auto.ria.com';
 const HEADERS  = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36';
 
@@ -12,15 +13,10 @@ const CONFIG = {
 };
 
 const getHtml = async (URL, CONFIG)=>{
-    try{
         const res = await axios.get(URL,CONFIG);
         const data = res.data;
         // console.log(data);
-        return data;
-    }
-    catch (e) {
-        console.error(e);
-    }
+    return data;
 }
 
 
@@ -30,66 +26,30 @@ const getContent = async()=>{
     const $ = cheerio.load(html);
     $('.na-gallery-view  .na-card-item').each((i,elem) => {
         uah_price = $(elem).find('.size15').text()
-        // if(uah_price == ''){
-        //     uah_price = $(elem).find('.size15').text()
-        // }
-        // else{
-        //     uah_price = 'Уточняйте'
-        // };
+        if(uah_price == ''){
+            uah_price = 'Уточняйте'
+        }
+        else{
+            uah_price = $(elem).find('.size15').text().replace(' • ', '')
+        }
         parse.push({
-            uah_price: $(elem).find('.size15').text().replace(' • ', ''),
+            uah_price: uah_price,
             title: $(elem).find('.size18').text(),
             link: HOST + $(elem).attr('href')
         })
     })
-    console.log(parse);
+    return parse;
+    // console.log(parse);
 }
 
-getContent();
+const writeExcel = async()=>{
+    console.log("Выполняется");
+    const xls = await getContent();
+    const json = json2xls(xls);
+    fs.writeFileSync('data.xlsx', json, 'binary');
+}
+
+writeExcel();
 
 
-// const getHtml = async (URL, HEADERS)=>{
-//     
-
-//     return await axios.get(URL,config)
-//         .then((response)=>{
-//             // console.log(response.data)
-//         })
-//         .catch((error)=> {
-//         // handle error
-//             console.log(error);
-//         })
-// }
-
-// const html = getHtml(URL, HEADERS);
-
-// console.log(html);
-
-// const parse = ()=>{
-//     const html = getHtml(URL, HEADERS);
-//     // if(html.status = 200){
-//     //     console.log("200");
-//     // }
-//     // else {
-//     //     console.log('else')
-//     // }
-//     console.log(html);
-// }
-
-// parse();
- 
-
-//  console.log(response.data);
-
-// let getData = (html) =>{
-//     data =[];
-//     const $ = cheerio.load(html);
-//     $('table table tr td:nth-child(3)').each((i,elem) => {
-//         data.push({
-//             title: $(elem).text(),
-//             link: $(elem).find('a.storylink').attr('href')
-//         })
-//     })
-//     console.log(data);
-// }
 
